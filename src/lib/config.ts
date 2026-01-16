19a65bfb0c2c6ade4ee3e615b9919f84c32e05a6
christurgeon <turgeonchris3@outlook.com>
2026-01-15 21:47:42 -0500

    Add Additional Photos and Clean up Text

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,16 +2,16 @@ export const siteConfig = {
   name: "Chris Turgeon",
   title: "Home",
   description:
-    "I am a software engineer at Point72. Outside of work, I spend my time traveling, photographing the world around me, reading books, tinkering with personal projects, and occasionally producing barely-listenable electronic music.",
+    "I am a software engineer on the infrastructure automation team at Point72, building software that quietly keeps everything else running. Outside of work, I spend my time traveling, photographing the world around me, reading books, tinkering with personal projects, and occasionally producing barely-listenable electronic music.",
   url: "https://christurgeon.com",
 
   // Social links
   socials: {
+    email: "turgechr@duck.com",
     github: "https://github.com/christurgeon",
     linkedin: "https://www.linkedin.com/in/chris-turgeon-29b42a151",
     goodreads: "https://www.goodreads.com/user/show/132072198-chris-turgeon",
     soundcloud: "https://soundcloud.com/technically-techo",
-    email: "turgechr@duck.com",
     beli: "https://beliapp.co/app/christurgeon",
   },
 
